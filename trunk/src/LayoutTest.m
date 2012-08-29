e53cc7cea393a2f0db3f9fed30f60d42c6fcc867
rwbowdidge@gmail.com <rwbowdidge@gmail.com@eab13115-e324-4862-c895-1835f8a8b02b>
2012-08-29 08:54:47 +0000

    Switch tests to flag lack of a yard before trying to see if the null pointer is a yard.

--- trunk/src/LayoutTest.m
+++ trunk/src/LayoutTest.m
@@ -251,8 +251,8 @@
 - (Yard*) yardAtStation: (NSString*) stationName {
 	InduYard *yard = [entireLayout_ industryWithName: [NSString stringWithFormat: @"%@-yard", stationName]
 									 withStationName: stationName];
-	STAssertTrue([yard isYard], @"Yard is not yard");
 	STAssertNotNil(yard, @"yardAtStation: no yard in %@", stationName);
+	STAssertTrue([yard isYard], @"Yard is not yard");
 	return (Yard*) yard;
 }
 
