fd9e9a92ef494bd0c3f0b0c20b89669836b4ee0c
rwbowdidge@gmail.com <rwbowdidge@gmail.com@eab13115-e324-4862-c895-1835f8a8b02b>
2012-09-22 05:24:44 +0000

    Create a file menu for opening, creating, and deleting files. Nothing hooked up yet.

--- trunk/src/iPad/FileCell.m
+++ trunk/src/iPad/FileCell.m
@@ -1,8 +1,8 @@
 //
-//  AppDelegate.h
-//  SwitchList for iPad
+//  FileCell.m
+//  SwitchList
 //
-//  Created by Robert Bowdidge on 8/30/12.
+//  Created by Robert Bowdidge on 9/21/12.
 //  Copyright (c) 2012 Robert Bowdidge. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -26,37 +26,26 @@
 // OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 // SUCH DAMAGE.
 
+#import "FileCell.h"
 
-#import <UIKit/UIKit.h>
+@implementation FileCell
 
-#import "EntireLayout.h"
-#import "LayoutController.h"
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
 
-@class MainWindowViewController;
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
 
-// Application-level support for the whole SwitchList-on-iPad app.
-// Contains functionality for looking at the current layout,
-// and for any interactions between different portions of the program.
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+    // Configure the view for the selected state
+}
 
-// Returns list of accessible filenames containing layouts.
-- (NSArray*) allLayouts;
-
-@property (strong, nonatomic) UIWindow *window;
-
-// Reference to TabBarController, used by MainController to open the tab bar.
-@property (strong, nonatomic) UITabBarController *layoutDetailTabBarController;
-
-// Wrapper object around the data file's store.
-@property (strong, nonatomic) EntireLayout *entireLayout;
-// Controller for changing the state of the layout - advancing to the next day, creating
-// cargos, etc.
-@property (strong, nonatomic) LayoutController *layoutController;
-
-// Name of preferred HTML style for drawing switchlists.
-// TODO(bowdidge): Make persistent.
-@property (retain, nonatomic) NSString *preferredTemplateStyle;
-
-// Handle to main view controller for reloading data / reprocessing trains.
-@property (retain, nonatomic) MainWindowViewController *mainWindowViewController;
+@synthesize label;
+@synthesize imageView;
 @end
