d058422bba8abbbb745a0ac55c0c83253b2f0e9b
rwbowdidge@gmail.com <rwbowdidge@gmail.com@eab13115-e324-4862-c895-1835f8a8b02b>
2014-08-22 05:22:54 +0000

    Code for layout graph.

---
+++ trunk/src/LayoutGraphView.h
@@ -0,0 +1,34 @@
+//
+//  LayoutGraphView.h
+//  SwitchList
+//
+//  Created by Robert Bowdidge on 2/16/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class LayoutGraph;
+
+@interface LayoutGraphView : UIView {
+    // True if a drag is currently being done to set a route.
+    BOOL performing_drag_;
+    // Current point for finger.
+    CGPoint current_drag_point_;
+
+    // List of stops indicated so far, in visit order.
+    NSMutableArray *current_selected_stops_;
+    
+    LayoutGraph *layout_graph_;
+    NSArray *all_stations_display_order_;
+    BOOL graph_initialized_;
+    
+    // Index of last station that was officially a stop.  Lines draw from this station.
+    int lastStationIndexTouched_;
+    // Index of current station that we think we're lingering on.  Used to invalidate the
+    // timer if the user moves away from this station.
+    int lingering_station_;
+    // Timer waiting for interval to confirm user wants current station to be on list of stops.
+    NSTimer *current_linger_timer_;
+}
+@end
---
+++ trunk/src/UILayoutGraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  UILayoutGraphViewController.h
+//  SwitchList
+//
+//  Created by Robert Bowdidge on 2/16/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILayoutGraphViewController : UIViewController
+
+@end
